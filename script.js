c1bcac7e5247fb0ba135dcff9578de7a3f29b286
Matthew Torres <mtorres@sitezeus.com>
2023-01-26 09:03:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ let weather = {
         "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp_convert + "°F";
-        document.querySelector(".humidity").innerText = "Humidity:" + humidity;
-        document.querySelector(".wind").innerText = "Wind Speed" + speed + " km/h";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity;
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
 
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
